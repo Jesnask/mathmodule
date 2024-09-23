4c2ac72b8d8d03bf04f752c67d97659856b1558f
Jesnask <jesnaskumar199@gmal.com>
2024-09-23 14:25:31 +0530

    first commit

---
+++ pythonmodu.py
@@ -0,0 +1,18 @@
+import math
+
+
+example_variable=math.sqrt(16)
+power_variable=math.pow(2,5)
+# ceil_variable=math.ceil(x=4, y=8)
+floor_variable=math.floor
+fabs_variable=math.fabs(-9)
+factorial_variable=math.factorial(4)
+sin_variable=math.sin(math.pi/2)
+cos_variable=math.cos(math.pi/2)
+tan_variable=math.tan(math.pi/2)
+log2_variable=math.log2(3)
+degree_variable=math.degree(math.pi/2)
+radian_variable=math.radian(90)
+
+
+print(radian_variable)
